39eeab5ffd221cdefe1a5e4e5bb711365dbcc171
frebarcelos <96388323+frebarcelos@users.noreply.github.com>
2024-04-18 23:50:37 -0300

    feat: criado novo projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# orvil
- Projeto modulo 3  - Analise e desenvolvimento de sistemas 
--- backend/controller/UserController.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ..view.UserView import createUser, login
-
-urlpatterns = [
-    path('cadastrar/', createUser, name='createUser'),
-    path('login/', login, name='login'),
-]
